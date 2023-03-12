d2df3fb80f4e0dbf3953481c4806faac7916d91e
Bunny181203 <115463799+Bunny181203@users.noreply.github.com>
2023-03-12 17:59:06 +0530

    Update myRISCVSim.h

--- myRISCVSim.h
+++ myRISCVSim.h
@@ -277,30 +277,37 @@ void decode(int IR[],int *arguments)
     {
         for(int i=11;i>=0;i--)
         {
-            imm[i]=IR[20+i]
+            imm[i]=IR[20+i];
         }
         Imm=bintodec(imm,12); 
         if(fun3==0){
             //lb
             operation=13;
-            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,,RD,Imm);
+            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,RD,Imm);
             
         }
         else if(fun3==1){
             //lh
             operation=14;
-            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,,RD,Imm);
+            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,RD,Imm);
         }
         else if(fun3==2){
             //lw
             operation=15;
-            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,,RD,Imm);
+            printf("Operation: load rs1: %d rd: %d imm: %d",RS1,RD,Imm);
         }
     }
     else if(Opcode==103)
     {
+        for(int i=11;i>=0;i--)
+        {
+            imm[i]=IR[20+i];
+        }
+        Imm = bintodec(imm,12);
         if(fun3==0){
             //jalr
+            operation=16;
+            printf("Operation: jalr rs1: %d rd: %d imm: %d",RS1,RD,Imm);
         }
     }
     else if(Opcode==35)
@@ -317,18 +324,18 @@ void decode(int IR[],int *arguments)
         Imm=bintodec(imm,12);
         if(fun3==0){
             //sb
-            operation=16;
-            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,,RS2,Imm);       
+            operation=17;
+            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,RS2,Imm);       
         }
         else if(fun3==1){
             //sh
-            operation=17;
-            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,,RS2,Imm);       
+            operation=18;
+            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,RS2,Imm);       
         }
         else if(fun3==2){
             //sw
-            operation=18;
-            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,,RS2,Imm);       
+            operation=19;
+            printf("Operation: store rs1: %d rs2: %d imm: %d",RS1,RS2,Imm);       
         }
     }
     else if(Opcode==101)
